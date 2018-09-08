5b5c08de16d8a0e85b68f3268dff1cb7b08ca52d
Kees Verhaar <kverhaar@gmail.com>
2018-09-08 16:00:07 +0200

    Use variables and "address-of" operator instead of new keyword. This releases us of memory management mumbo-jumbo.

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,39 @@
 const char *ssid = "";
 const char *password = "";
 
-Settings* settings = new Settings();
+Settings settings;
 Screen screen;
 MqttClient mqttClient;
-RainmanStatus *status = new RainmanStatus();
+RainmanStatus status;
+
+WateringStation station1;
+WateringStation station2;
+WateringStation station3;
+WateringStation station4;
+WateringStation station5;
+WateringStation station6;
 
 std::vector<WateringStation *> wateringStations{
-    new WateringStation(),
-    new WateringStation(),
-    new WateringStation(),
-    new WateringStation(),
-    new WateringStation(),
-    new WateringStation()};
+    &station1,
+    &station2,
+    &station3,
+    &station4,
+    &station5,
+    &station6};
 
 void setup()
 {
     Serial.begin(115200);
-    settings->Begin();
+    settings.Begin();
 
-    wateringStations[0]->Setup(1, D0);
-    wateringStations[1]->Setup(2, D1);
-    wateringStations[2]->Setup(3, D2);
-    wateringStations[3]->Setup(4, D3);
-    wateringStations[4]->Setup(5, D4);
-    wateringStations[5]->Setup(6, D5);
+    station1.Setup(1, D0);
+    station2.Setup(2, D1);
+    station3.Setup(3, D2);
+    station4.Setup(4, D3);
+    station5.Setup(5, D4);
+    station6.Setup(6, D5);
 
-    screen.Start(status, wateringStations);
+    screen.Start(&status, wateringStations);
 
     /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
     would try to act as both a client and an access-point and could cause
@@ -50,26 +57,26 @@ void setup()
         Serial.print(".");
     }
 
-    settings->DumpToSerial();
+    settings.DumpToSerial();
 
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
-    start_webserver(settings);
-    mqttClient.Start(settings, status, wateringStations);
+    start_webserver(&settings);
+    mqttClient.Start(&settings, &status, wateringStations);
 }
 
 void handle_status()
 {
     if (WiFi.status() != WL_CONNECTED)
     {
-        status->SetWifiStatus(t_RainmanConnectionStatus::ConnectionNOK);
+        status.SetWifiStatus(t_RainmanConnectionStatus::ConnectionNOK);
     }
     else
     {
-        status->SetWifiStatus(t_RainmanConnectionStatus::ConnectionOK);
+        status.SetWifiStatus(t_RainmanConnectionStatus::ConnectionOK);
     }
 }
 
